d1745788495a174863f66621b8601e6b48b85a4f
3cxc <81449257+3cxc@users.noreply.github.com>
2024-05-19 00:56:36 +0800

    初始代码

---
+++ src/main/java/com/github/basiclib/Configuration/ConfigInterface.java
@@ -0,0 +1,16 @@
+package com.github.basiclib.Configuration;
+
+/**
+ * 通用的配置文件接口
+ * <p>
+ * 以确保适配多个不同的配置文件管理器
+ * @since Plugin Basic Lib 1.0
+ * @author 3cxc
+ * @version 1.1
+ */
+public interface ConfigInterface {
+    String getPath();
+    void reloadConfig();
+    void saveConfig();
+    boolean exists();
+}
---
+++ src/main/java/com/github/basiclib/util/FileUtils.java
@@ -0,0 +1,99 @@
+package com.github.basiclib.util;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * 一个简易的文件操作类
+ * @since Plugin Basic Lib 1.0
+ * @author 3cxc
+ * @version 1.1
+ */
+public class FileUtils {
+
+    /**
+     * 返回程序所在的目录路径
+     * @return 程序目录路径
+     */
+    public static @Nullable String getDataFolder(){//提供程序所在路径
+        File file = new File(".");
+        String path; //程序路径
+        try {
+            path = file.getCanonicalPath() + "\\";
+            return path; //返回
+        }catch (IOException e){
+            return null; //抛出null
+        }
+    }
+
+    /**
+     * 从指定的类加载器读取文件
+     * @param resourcePath JAR包内相对路径
+     * @param classLoader 指定的类加载器
+     * @return 文件内容
+     * @throws IOException 如果读取发生错误
+     */
+    public static String readJar(String resourcePath, @NotNull ClassLoader classLoader) throws IOException{
+        // 使用ClassLoader的getResourceAsStream方法获取资源的输入流
+        InputStream inputStream = classLoader.getResourceAsStream(resourcePath);
+
+        // 使用InputStreamReader和BufferedReader读取输入流中的内容
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        // 读取文件内容
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            stringBuilder.append(line);
+        }
+        String tmp = stringBuilder.toString();
+        // 关闭流
+        bufferedReader.close();
+        inputStreamReader.close();
+        inputStream.close();
+        //返回文件内容
+        return tmp;
+    }
+
+    /**
+     * 获取指定文件夹内所有指定后缀的文件的绝对路径
+     * @param folderPath 文件夹路径
+     * @param suffix 要查找的文件的后缀
+     * @return 包含所有指定后缀的文件的绝对路径的列表或null(找不到文件夹)
+     */
+    public static @NotNull List<String> getJarFilePaths(String folderPath,String suffix) {
+        // 创建存储.jar文件绝对路径的列表
+        List<String> jarFilePaths = new ArrayList<>();
+
+        // 创建文件夹对象
+        File folder = new File(folderPath);
+
+        // 检查文件夹是否存在,不存在直接返回空列表
+        if (!folder.isDirectory()) {
+            return jarFilePaths;
+        }
+
+        // 获取文件夹内的所有文件
+        File[] files = folder.listFiles();
+
+        // 遍历文件列表
+        if (files != null) {
+            for (File file : files) {
+                // 判断文件后缀是否一致
+                if (file.isFile() && file.getName().endsWith(suffix)) {
+                    // 将该文件的绝对路径添加到列表中
+                    jarFilePaths.add(file.getAbsolutePath());
+                }
+            }
+        }
+
+        // 返回包含所有指定文件绝对路径的列表
+        return jarFilePaths;
+    }
+}
