3f9ec2f8c1e47f8e55066ff4f565459cb9159bea
3cxc <81449257+3cxc@users.noreply.github.com>
2024-09-10 23:05:32 +0800

    修复无法正常存储启动类的问题
    Signed-off-by: 3cxc <81449257+3cxc@users.noreply.github.com>

--- src/java/com/github/tlib/init/InitManager.java
+++ src/java/com/github/tlib/init/InitManager.java
@@ -5,12 +5,12 @@ import com.google.common.collect.MutableClassToInstanceMap;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * 一个简单的 加载/运行/结束 任务调度器
+ * 一个简单的任务调度器
  * <p>
  * 可以让多个程序使用统一的任务调度流程
  * @since ToolboxLibrary 1.1
  * @author 3cxc
- * @version 1.1
+ * @version 1.2
  */
 @SuppressWarnings("unused")
 public class InitManager {
@@ -20,41 +20,24 @@ public class InitManager {
     final private ClassToInstanceMap<Initable> initableClassToStopInstanceMap = MutableClassToInstanceMap.create();
 
     /**
-     * 向Map存储一个需要在加载阶段调用的类
-     * <p>
-     * 注意:Map是顺序遍历，即顺序调用类
+     * 向Map存储一个需要在加载阶段调用的类(Map是顺序遍历的)
      * @param key 继承{@link Initable}的类
      * @param value 继承{@link Initable}的类的实例，应该与key一致
      * @since ToolboxLibrary 1.1
      * @author 3cxc
      */
-    public void putLoadMethodToMap(Class<Initable> key, Initable value){
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public void putLoadMethodToMap(Class key, Initable value){
         initableClassToLoadInstanceMap.put(key,value);
     }
 
-    /**
-     * 向Map存储一个需要在运行阶段调用的类
-     * <p>
-     * 注意:Map是顺序遍历，即顺序调用类
-     * @param key 继承{@link Initable}的类
-     * @param value 继承{@link Initable}的类的实例，应该与key一致
-     * @since ToolboxLibrary 1.1
-     * @author 3cxc
-     */
-    public void putRunMethodToMap(Class<Initable> key, Initable value){
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public void putRunMethodToMap(Class key, Initable value){
         initableClassToRunInstanceMap.put(key,value);
     }
 
-    /**
-     * 向Map存储一个需要在停止阶段调用的类
-     * <p>
-     * 注意:Map是顺序遍历，即顺序调用类
-     * @param key 继承{@link Initable}的类
-     * @param value 继承{@link Initable}的类的实例，应该与key一致
-     * @since ToolboxLibrary 1.1
-     * @author 3cxc
-     */
-    public void putStopMethodToMap(Class<Initable> key, Initable value){
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public void putStopMethodToMap(Class key, Initable value){
         initableClassToStopInstanceMap.put(key,value);
     }
 
@@ -68,7 +51,7 @@ public class InitManager {
         executeMethod(this.initableClassToStopInstanceMap);
     }
 
-    public void executeMethod(final @NotNull ClassToInstanceMap<Initable> map){
+    private void executeMethod(final @NotNull ClassToInstanceMap<Initable> map){
         for (Initable initable : map.values()){
             initable.run();
         }
